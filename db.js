1eb113330950e7f74ff70ed2fbc9365ea6995e31
SanketDeshmukh007 <sanketdeshmukh2412@gmail.com>
2024-08-05 19:28:39 +0530

    Initial Commit

---
+++ db.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoURL = process.env.Local_URL;
+// const mongoURL = process.env.DB_URL;
+
+// Set up MongoDB connection
+mongoose.connect(mongoURL)
+
+// Get the default connection
+// Mongoose maintains a default connection object representing the MongoDB connection.
+const db = mongoose.connection;
+
+// Define event listeners for database connection
+
+db.on('connected', () => {
+    console.log('Connected to MongoDB server');
+});
+
+db.on('error', (err) => {
+    console.error('MongoDB connection error:', err);
+});
+
+db.on('disconnected', () => {
+    console.log('MongoDB disconnected');
+});
+
+// Export the database connection
+module.exports = db;
